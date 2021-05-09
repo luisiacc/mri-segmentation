76b8b3e0d338802ab1a23235e81bf436f4f2e130
luisiacc <lccpacc@gmail.com>
2021-05-09 01:38:14 -0400

    add helper functions for categorizing dicom slices

--- core/dicom_processing/utils.py
+++ core/dicom_processing/utils.py
@@ -1,10 +1,36 @@
+from io import BytesIO, FileIO
+from pathlib import Path
+from typing import BinaryIO, Iterable, Union
+
+from django.core.files.storage import default_storage
+from django.db.models.enums import TextChoices
+
 import numpy as np
 import png
 import pydicom
+import rarfile
+
+DEFAULT_EXCLUDED_CUT = "T2 TSE Tra"
+
+FileLike = Union[BytesIO, BinaryIO, FileIO, rarfile.PipeReader, rarfile.DirectReader]
 
 
-def dicom2png(source_file, output_file):
-    ds = pydicom.dcmread(source_file)
+class Cut(TextChoices):
+    Sagittal = "sagittal"
+    Coronal = "coronal"
+    Axial = "axial"
+    Unknown = "unkown"
+
+
+def open_dcm(file: Union[str, FileLike]):
+    if isinstance(file, FileLike.__args__):
+        file.seek(0)
+
+    return pydicom.dcmread(file)
+
+
+def dicom2png(source_file: Union[str, FileLike], output_file: str):
+    ds = open_dcm(source_file)
     shape = ds.pixel_array.shape
 
     # Convert to float to avoid overflow or underflow losses.
@@ -17,6 +43,39 @@ def dicom2png(source_file, output_file):
     image_2d_scaled = np.uint8(image_2d_scaled)
 
     # Write the PNG file
-    with open(output_file, "wb") as png_file:
-        w = png.Writer(shape[1], shape[0], greyscale=True)
-        w.write(png_file, image_2d_scaled)
+    new_file_stream = BytesIO()
+    w = png.Writer(shape[1], shape[0], greyscale=True)
+    w.write(new_file_stream, image_2d_scaled)
+    default_storage.save(output_file, new_file_stream)
+
+
+def get_slice_cut(dcm: Union[pydicom.Dataset, FileLike]) -> Cut:
+    if not isinstance(dcm, pydicom.Dataset):
+        dcm = open_dcm(dcm)
+
+    IOP: list = getattr(dcm, "ImageOrientationPatient", [])
+    assert len(IOP), "ImageOrientationPatient no puede ser una lista vacia"
+
+    IOP_round = [round(x) for x in IOP]
+    plane = np.cross(IOP_round[0:3], IOP_round[3:6])
+    plane = [abs(x) for x in plane]
+
+    if plane[0] == 1:
+        return Cut.Sagittal
+    elif plane[1] == 1:
+        return Cut.Coronal
+    elif plane[2] == 1:
+        return Cut.Axial
+
+    return Cut.Unknown
+
+
+def filter_by_series(dcm_list: Iterable[pydicom.Dataset], exclude=(DEFAULT_EXCLUDED_CUT,)) -> list[pydicom.Dataset]:
+    return [dcm for dcm in dcm_list if dcm.SeriesDescription not in exclude]
+
+
+def get_dcm_files_from_rarfile(rar_path: Union[str, Path]) -> Iterable[FileLike]:
+    rar = rarfile.RarFile(rar_path)
+    dcms = (dcm for dcm in rar.infolist() if dcm.filename.lower().endswith("dcm"))
+    for file in dcms:
+        yield rar.open(file)
